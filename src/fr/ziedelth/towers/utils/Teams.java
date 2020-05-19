ce4cb88e4d54d326bae9d82c5c919e7e144026e9
Ziedelth <watteaupierre@gmail.com>
2020-05-20 00:20:05 +0200

    Add Teams.java

---
+++ src/fr/ziedelth/towers/utils/Teams.java
@@ -0,0 +1,10 @@
+package fr.ziedelth.towers.utils;
+
+import org.bukkit.Location;
+
+public interface Teams {
+    String getName();
+    Location getSpawnLocation();
+    Location getPointFrom();
+    Location getPointTo();
+}
